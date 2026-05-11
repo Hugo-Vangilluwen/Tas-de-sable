747da070ce15d378eac39b6c8f8878e9aaf905d0
Quentinbll <regencepark83@gmail.com>
2026-05-11 21:30:12 +0200

    simulations configurecurentes

--- tas_sable/main.ml
+++ tas_sable/main.ml
@@ -12,7 +12,7 @@ module Tsp = Tas_sable(Ajouter_puit (Grille_carree))
 
 let main: unit =
 
-    let tas = Tsc.creer () (90, 90) in
+    (*let tas = Tsc.creer () (90, 90) in
     let (tot, r) = Tsc.avalanche_n_grains tas (25,25) 10050 in
     print_int r; print_string "\n"
     (*Tsc.deposer source 1 (15, 15);
@@ -26,7 +26,7 @@ let main: unit =
     let tas1 = Tsc.creer () (40, 40) in
     Tsc.mettre_tailles_cases 20;
     let tas2 = Tsc.animer tas1 1000 source attendre1 in
-    let tas3 = Tsc.animer tas2 500 source attendre2 in ()*)
+    let tas3 = Tsc.animer tas2 500 source attendre2 in ()*)*)
 
 
 
@@ -44,10 +44,10 @@ let main: unit =
         Tsc.identite () c |> Tsc.afficher
     done*)
 
-(*     let _ = Tsc.un_grain_temps () (Tscomplet.creer () (10, 0)) (4, 0) 123 0.05 in () *)
-(*     in Graphics.close_graph () *)
-(*     let id = Tsm.identite 0.5 (10, 10) in *)
-(*     Tsm.afficher id *)
+   (*      let _ = Tsc.un_grain_temps () (Tscomplet.creer () (10, 0)) (4, 0) 123 0.05 in ()
+        in Graphics.close_graph () *)
+        let id = Tsc.identite () (73, 73) in
+        Tsc.afficher id
 
     (*
     let source = Tsm.creer (20, 20) in
